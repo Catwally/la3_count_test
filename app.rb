9e30cbefcfd680483d352088d8c77a54222b7d21
Catwally <widebridge10275@gmail.com>
2022-02-14 05:46:35 +0000

    Implement feature: creating new counter

--- app.rb
+++ app.rb
@@ -7,4 +7,52 @@ before do
   if Count.count == 0
     Count.create(number: 0)
   end
+  
+end
+
+get '/' do
+  @number = Count.first.number
+  @number2 = Count.first.number
+  erb :index
+  
+end
+
+
+post '/plus' do
+  count = Count.first
+  count.number = count.number + 1
+  count.save
+  redirect '/'
+end
+post '/plus2' do
+  count = Count.first
+  count.number = count.number + 1
+  count.save
+  redirect '/'
+end
+
+post '/minus' do
+  count = Count.first
+  count.number = count.number - 1
+  count.save
+  redirect '/'
+end
+post '/clear' do
+  count = Count.first
+  count.number = 0
+  count.save
+  redirect '/'
+end
+
+post '/multi' do
+  count = Count.first
+  count.number = count.number * 2
+  count.save
+  redirect '/'
+end
+post '/divide' do
+  count = Count.first
+  count.number = count.number / 2
+  count.save
+  redirect '/'
 end
